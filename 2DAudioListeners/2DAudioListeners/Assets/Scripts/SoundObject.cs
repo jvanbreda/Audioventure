d6f5ee219927c9d9379419326808295748fcda45
Yonita van Oudheusden <yonitavo@hotmail.com>
2016-11-10 11:40:02 +0100

    2DAudioListeners
    The 2DAudioListeners project based on a 2D environment.

---
+++ 2DAudioListeners/2DAudioListeners/Assets/Scripts/SoundObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts {
+    public class SoundObject : MonoBehaviour {
+        [SerializeField]
+        public AudioSource audioSource;
+        [SerializeField]
+        public int angle;
+        const float volumeBoost = 2;
+
+        public virtual void Init() { }
+
+        public void UpdateAudioSource(float currentCameraAngle) {
+            UpdateVolume(currentCameraAngle);
+            UpdatePan(currentCameraAngle);
+        }
+
+        protected void UpdatePan(float currentCameraAngle) {
+            float angleDifference = -1 * (currentCameraAngle - angle);
+            float pan = angleDifference / 180.1f;
+            // TODO: pan cannot be > 1 || < -1
+            //Debug.Log("Pan: " + pan);
+            SetPan(pan);
+        }
+
+        protected void UpdateVolume(float currentCameraAngle) {
+            if (!TimeChecker.audioMuted) {
+                float angleDifference = Math.Abs(currentCameraAngle - angle);
+                //Use angle to change volume
+                float newVolume = 1 - volumeBoost * (angleDifference / 180f);
+                newVolume = Math.Max(0, newVolume);
+                //Debug.Log("Volume: " + newVolume);
+                // set audio volume based on angle
+                SetVolume(newVolume);
+            }
+        }
+
+        // set audio 
+        public void SetVolume(float volume) {
+            audioSource.volume = volume;
+        }
+
+        // set panning 
+        public void SetPan(float panStereo) {
+            audioSource.panStereo = panStereo;
+        }
+
+        public float GetVolume() {
+            return audioSource.volume; 
+        }
+    }
+}
---
+++ 2DAudioListeners/2DAudioListeners/Assets/Scripts/TimeChecker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts {
+    public class TimeChecker : MonoBehaviour {
+        private SoundObject[] SoundObjectsArray;
+        const float MIN_TIME_LOOK = 5;
+        const float MAX_RANGE = 10;
+        SoundObject CurrentLookedAtGO = null;
+        double currentTimeLookedAt = 0;
+        VRCameraController vrc;
+        bool executed;
+        public static bool audioMuted;
+
+        public void Init(VRCameraController vrc) {
+            this.vrc = vrc;
+            SoundObjectsArray = vrc.soundObjects;
+            executed = false;
+            audioMuted = false;
+        }
+
+        public void Update() {
+            SoundObject current = GetLookedAtObject(vrc.currentCameraAngle);
+            Debug.Log("Test");
+            if (current != CurrentLookedAtGO) {
+                currentTimeLookedAt = 0;
+                executed = false;
+                audioMuted = false;
+                // Reset de waarde omdat je er niet meer naar kijkt;
+                CurrentLookedAtGO = current;
+            } else {
+                currentTimeLookedAt += Time.deltaTime;
+            }
+
+            if (currentTimeLookedAt > MIN_TIME_LOOK) {
+                audioMuted = true;
+                // TODO: Als de currentTimeLookedAt groter is dan 5; voer deze functie uit;
+                if (!executed) {
+                    foreach (SoundObject source in SoundObjectsArray) {
+                        if (!source.Equals(current)) {
+                            //StartCoroutine(AudioFadeOut.FadeOut(source.audioSource, 5f));   
+                            source.SetVolume(0.1f);
+                        }
+                    }
+                    executed = true;
+                }
+            }
+        }
+
+        private SoundObject GetLookedAtObject(float CurrentCameraAngle) {
+            foreach (SoundObject source in SoundObjectsArray) {
+                float AngleDifference = Math.Abs(CurrentCameraAngle - source.angle);
+                if (AngleDifference < MAX_RANGE) {
+                    return source;
+                }
+            }
+            return null;
+        }
+    }
+}
