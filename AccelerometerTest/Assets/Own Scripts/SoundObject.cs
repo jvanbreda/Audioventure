39d50ec1f8c11360f913162883ece1fb5451a7ee
Yonita van Oudheusden <yonitavo@hotmail.com>
2016-11-18 16:06:45 +0100

    Fixed: pickupsound now working properly

--- AccelerometerTest/Assets/Own Scripts/SoundObject.cs
+++ AccelerometerTest/Assets/Own Scripts/SoundObject.cs
@@ -53,7 +53,7 @@ namespace Assets {
         public void UpdateAudioSource() {
             if (cc.counter == index) {
                 audioSource.enabled = true;
-                if (!audioSource.isPlaying && !GameObject.Find("EndSound").GetComponent<AudioSource>().isPlaying)
+                if (!audioSource.isPlaying && !GameObject.Find("EndSound").GetComponent<AudioSource>().isPlaying && !GameObject.Find("PickupSound").GetComponent<AudioSource>().isPlaying)
                     audioSource.Play();
                 UpdateVolume();
                 UpdatePan();
@@ -91,7 +91,7 @@ namespace Assets {
 
         private void CheckCollision() {
             if (audioModel.distance < 5 && cc.counter == index) {
-                GameObject.Find("CoinSound").GetComponent<AudioSource>().Play();
+                GameObject.Find("PickupSound").GetComponent<AudioSource>().Play();
                 audioSource.enabled = false;
                 cc.counter++;
             }
