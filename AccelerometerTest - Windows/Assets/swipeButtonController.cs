432ad4c00122fe21d165bbb2d441405a0da0ae1c
Yonita van Oudheusden <yonitavo@hotmail.com>
2016-11-23 10:55:09 +0100

    Fixed: Button Clicks for smartphone - New windows build

---
+++ AccelerometerTest - Windows/Assets/swipeButtonController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+using UnityEngine.SceneManagement;
+
+public class swipeButtonController : MonoBehaviour, IPointerDownHandler {
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Button swipeButton = GameObject.Find("SwipeButton").GetComponent<Button>();
+        SceneManager.LoadScene("AccelerometerTest");
+        Debug.Log("Test");
+
+        swipeButton.GetComponent<Image>().color = Color.blue;
+    }
+}
